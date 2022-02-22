be6d76b76ebf7d9037754fce87a4f3f3f85679d0
LeninJimenezTorres <jlenin25@hotmail.com>
2022-02-21 23:22:56 -0500

    V1.3 Remote query

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var TRACKID, DRIVER, LONGITUDE, LATITUDE;
+
+const remoteRepo='https://apigw.withoracle.cloud/formulaai/sessions';
+
+
+var https = require('https');
+https.get(remoteRepo,(res) => {
+  let body1 = "";
+  res.on("data", (chunk) => {
+      body1 += chunk;
+  });
+  res.on("end", () => {
+      try {
+          json1 = JSON.parse(body1);
+          console.log("Archivo guardado");
+          console.log('Longitud de archivo: '+json1.length);
+        }
+      catch (error) {
+          console.error(error.message);
+      };
+  });
+});
+
 
 /* GET home page. */
 router.get('/trackid/:t_id/', function(req, res, next){
   //res.send('Hello World!');
   //res.render('index', { title: 'Express' });
+  /*
   res.json(
     {
       "trackId": req.params.t_id,
@@ -13,6 +37,17 @@ router.get('/trackid/:t_id/', function(req, res, next){
       "latitude": 5678659
     }
   )
-});
+  */
+  var output=[];
+  if(json1){
+    for(var i=0; i<json1.length; i++){
+      if(json1[i].TRACKID==req.params.t_id)
+      {
+        output.push(json1[i]);   
+      }
+    }
+    res.json( output )
+  }
+})
 
 module.exports = router;
